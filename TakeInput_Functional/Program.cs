387dca3b0db77e80cd6f6743a1baaa52184f2a92
Rusted Wizard <chensike89@outlook.com>
2021-03-09 15:39:05 -0800

    NotNull attribute

--- TakeInput_Functional/Program.cs
+++ TakeInput_Functional/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace TakeInput_Functional
@@ -10,7 +11,7 @@ namespace TakeInput_Functional
         //way. As showing in this example, there no variable used.
 
         //Let user decide what function should be used to take input
-        static IEnumerable<string> TakeInput(Func<string> function)
+        static IEnumerable<string> Take(Func<string> function)
         {
             //Keep taking user input.
             //let user decide when to stop
@@ -19,15 +20,25 @@ namespace TakeInput_Functional
                 //apply user specified function and yield return
                 yield return function();
             }
+            // ReSharper disable once IteratorNeverReturns
         }
 
-        static void Main(string[] args)
+        static IEnumerable<string> TakeInput([NotNull]Func<string> inputSource, [NotNull]Func<string, bool> stop)
+        {
+            return Take(inputSource).TakeWhile(stop);
+        }
+
+        static void Main()
         {
             //in this case use Console.ReadLine function
             //TakeWhile enable the specification of function which determines when to stop taking input
             //in this case when input is an empty line, program should stop taking input
             //in this case all taken input will be convert to list.
-            var list = TakeInput(Console.ReadLine).TakeWhile(x => !x.Trim().Equals("")).ToList();
+            var list = TakeInput(Console.ReadLine, x=>!x.Trim().Equals("")).ToList();
+            foreach(var e in list)
+            {
+                Console.WriteLine($"You entered: {e}");
+            }
         }
     }
 }
